4e25a967e6ac24570b821afad9df757af8f58b0a
Derek Alsop <livewire192@gmail.com>
2024-05-23 11:43:24 -0700

    fix: Sandbox lua

--- DrawingAPI/Lua.cs
+++ DrawingAPI/Lua.cs
@@ -9,11 +9,20 @@ public class Lua
     public static int Handle(string code)
     {
         using var state = new NLua.Lua();
-        state.DoString("import = function () end");
         state.RegisterFunction("measure", typeof(Lua).GetMethod("Measure"));
-        var value = state.DoString(code);
-        if (value[0] == null) throw new Exception("Nothing was returned from lua");
-        return Convert.ToInt32(value[0]);
+        var value = state.DoString($$"""
+                         local env = {
+                           measure = measure
+                         }
+                         local function run(untrusted_code)
+                           local untrusted_function, message = load(untrusted_code, nil, 't', env)
+                           if not untrusted_function then return nil, message end
+                           return pcall(untrusted_function)
+                         end
+                         return run [[{{code}}]]
+                         """);
+        if (value[0] == null || (bool)value[0] == false) throw new Exception("Nothing was returned from lua");
+        return Convert.ToInt32(value[1]);
     }
 
     public static Size Measure(string text, int size, string font, float wrapWidth = -1)
